e2919a77663177763b5c82af59bca1d30b0359d4
Jan Wedekind <jan@wedesoft.de>
2010-12-24 10:31:12 +0000

    Initial release

---
+++ README.md
@@ -0,0 +1,37 @@
+ruby-cwiid
+==========
+
+**Author**:       Jan Wedekind
+**Copyright**:    2010
+**License**:      GPL
+
+Synopsis
+--------
+
+This Ruby extension provides access to a Wii Remote.
+
+Installation
+------------
+
+To install this Ruby extension, use the following command:
+
+    $ sudo gem install ruby-cwiid
+
+Alternatively you can build and install the Ruby extension from source as follows:
+
+    $ rake
+    $ sudo rake install
+
+Usage
+-----
+
+Simply run Interactive Ruby:
+
+    $ irb
+
+You can load the Ruby extension like this:
+
+    require 'rubygems'
+    require 'ruby-cwiid'
+
+
---
+++ ext/init.cc
@@ -0,0 +1,37 @@
+/* ruby-cwiid - Wii Remote interface.
+   Copyright (C) 2010 Jan Wedekind
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+#include "rubyinc.hh"
+
+#ifdef WIN32
+#define DLLEXPORT __declspec(dllexport)
+#define DLLLOCAL
+#else
+#define DLLEXPORT __attribute__ ((visibility("default")))
+#define DLLLOCAL __attribute__ ((visibility("hidden")))
+#endif
+
+extern "C" DLLEXPORT void Init_cwiid(void);
+
+extern "C" {
+
+  void Init_cwiid(void)
+  {
+    // VALUE rbHornetseye = rb_define_module( "Hornetseye" );
+    // Malloc::init( rbHornetseye );
+    rb_require( "cwiid_ext.rb" );
+  }
+
+}
